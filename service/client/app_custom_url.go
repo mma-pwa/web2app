70dadbdb09974241c63fc07e0913818a117d1f18
10804 <108048451@qq.com>
2024-04-09 11:32:00 +0800

    域名修改

--- service/client/app_custom_url.go
+++ service/client/app_custom_url.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"errors"
+	"strings"
 	"time"
 	"web2app/global"
 	"web2app/model"
@@ -22,6 +23,7 @@ func (s *AppCustomURLService) CreateAppCustomURL(appCustom model.AppCustomURL) e
 	if appCustom.DomainPrefix == "." || appCustom.DomainPrefix == "@" {
 		return errors.New("domain prefix error")
 	}
+	appCustom.CustomURL = appCustom.DomainPrefix + "." + appCustom.CustomURL
 	return global.GVA_DB.Create(&appCustom).Error
 }
 func (s *AppCustomURLService) DeleteAppCustomURL(appCustom model.AppCustomURL) error {
@@ -39,7 +41,7 @@ func (s *AppCustomURLService) GetAppCustomList(info request.AppCustomURLSearch) 
 	limit := info.PageSize
 	offset := info.PageSize * (info.Page - 1)
 	db := global.GVA_DB.Model(&model.AppCustomURL{})
-	var appList []model.AppCustomURLRes
+	appList := make([]*model.AppCustomURLRes, 0)
 
 	err = db.Count(&total).Error
 	if info.UserId != "" {
@@ -49,5 +51,8 @@ func (s *AppCustomURLService) GetAppCustomList(info request.AppCustomURLSearch) 
 		"app_custom_url.updated_at, app_custom_url.app_id,a.name").
 		Joins("left join app as a on a.id=app_custom_url.app_id ", info.UserId).Scan(&appList).Limit(limit).Offset(offset).Error
 	//err = db.Limit(limit).Offset(offset).Order("updated_at desc").Find(&appList).Error
+	for _, v := range appList {
+		v.CustomURL = strings.Replace(v.CustomURL, v.DomainPrefix+".", "", 1)
+	}
 	return err, appList, total
 }
